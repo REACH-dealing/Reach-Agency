e00dd205aafd39f581e71d774b45e1c3edfc4018
AymanEhab202 <aymanelghozy202@gmail.com>
2024-08-17 17:55:01 +0300

    Slider

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 50,
+  //   loop: true,
+
+  // If we need pagination
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  //   scrollbar: {
+  //     el: ".swiper-scrollbar",
+  //   },
+
+  breakpoints: {
+    // When window width is >= 768px
+    768: {
+      slidesPerView: 2, // Show 2 slides at a time
+      spaceBetween: 50,
+    },
+    // When window width is >= 992px
+    992: {
+      slidesPerView: 3, // Show 3 slides at a time
+      spaceBetween: 50,
+    },
+  },
+});
