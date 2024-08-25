b6dbf98eb51596680b30d1f3a3fd1c567c5ef18b
AymanEhab202 <aymanelghozy202@gmail.com>
2024-08-25 18:04:28 +0300

    about us page first and second sections

--- js/main.js
+++ js/main.js
@@ -34,3 +34,10 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+const navbar = document.querySelector(".links");
+const toggleIcone = document.querySelector(".toggleIcon");
+
+toggleIcone.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
